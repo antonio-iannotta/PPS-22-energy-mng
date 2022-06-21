b630325f01c52b265f6076663c428774b479a32a
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-06-21 21:31:04 +0200

    First commit: Bill and operationBill code

--- src/test/scala/UserBillTest.scala
+++ src/test/scala/UserBillTest.scala
@@ -1,4 +1,10 @@
-import org.scalatest.Assertions._
-class UserBillTest extends AssertionsHelper:
-  val testBill = UserBill("234","AntonioIannotta","private","water",234.65,89.6,
+import org.scalatest.funsuite.AnyFunSuite
+
+object UserBillTest:
+  class User extends AnyFunSuite:
+    val testBill = UserBill("234","AntonioIannotta","private","water",234.65,89.6,
     2,2022,"Cesena","Emilia-Romagna")
+
+    test("BillID should be equals to 234") {
+      assert(testBill.getBillID() == "234")
+    }
