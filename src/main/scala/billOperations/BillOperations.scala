d7ec65d34318dcb158caf2152e80463260b32161
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-06-24 17:47:06 +0200

    Commit: Refactoring codice BillOperations

--- src/main/scala/billOperations/BillOperations.scala
+++ src/main/scala/billOperations/BillOperations.scala
@@ -20,104 +20,221 @@ object BillOperations:
   )
 
 
-  def getIndividualUsage(userID: String, usageType: String): String =
-    var result = ""
-    val userIdBills = list.filter(bill => bill.getUserID() == userID && bill.getUsageType() == usageType)
+
+  /*
+  Il seguente metodo ritorna il costo o il consumo mensile per una certa utenza relativamente ad uno specifico utente
+  */
+  def getIndividualCostOrUsage(userID: String, usageType: String, costOrUsage: String): String =
+    var individualInformation = ""
+    val userIdBills = getBillsByUserIDAndUsageType(userID, usageType)
     for (bill <- userIdBills) do
-      result = "usage: " + bill.getUsage() +
-        "-month: " + bill.getMonth() +
-        "-year: " + bill.getYear() + "\n"
+      individualInformation += composeUsageOrCostInformation(bill, costOrUsage.toLowerCase())
 
-    result
+    individualInformation
 
+  /*
+  Il seguente metodo ritorna il costo o il consumo mensile per una certa utenza relativamente ad una certa località geografica
+  */
+  def getUsageOrCostByLocation(userType: String, usageType: String, cityRegion: String, cityOrRegion: String, usageOrCost: String): String =
+    monthlyUsageOrCost(userType,usageType,cityOrRegion,cityRegion,usageOrCost).toString()
 
-  def getIndividualCost(userID: String, usageType: String): String =
-    var result = ""
-    val userIdBills = list.filter(bill => bill.getUserID() == userID && bill.getUsageType() == usageType)
-    for (bill <- userIdBills) do
-      result = "usage: " + bill.getCost() + "\n" +
-        "month: " + bill.getMonth() + "\n" +
-        "year: " + bill.getYear()
-
-    result
-
-  def getUsageByLocation(userType: String, usageType: String, location: String, locationType: String): String =
-    var result = ""
-    val monthlyUsage: LinkedHashMap[Int, Double] = LinkedHashMap()
-    locationType match
-      case "city" =>
-        val locationBills = list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getCity() == location)
-        for i <- Range(1, 12) do
-          val monthlyUsageSum = locationBills.filter(bill => bill.getMonth() == i).foldLeft(0.0)(_ + _.getUsage())
-          monthlyUsage(i) = monthlyUsageSum
-      case "region" =>
-        val locationBills = list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getRegion() == location)
-        for i <- Range(1, 12) do
-          val monthlyUsageSum = locationBills.filter(bill => bill.getMonth() == i).foldLeft(0.0)(_ + _.getUsage())
-          monthlyUsage(i) = monthlyUsageSum
-
-    result = monthlyUsage.toString()
-    result
-
-
-  def getCostByLocation(userType: String, usageType: String, location: String, locationType: String): String =
-    var result = ""
-    val monthlyUsage: LinkedHashMap[Int, Double] = LinkedHashMap()
-    locationType match
-      case "city" =>
-        val locationBills = list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getCity() == location)
-        for i <- Range(1, 12) do
-          val monthlyUsageSum = locationBills.filter(bill => bill.getMonth() == i).foldLeft(0.0)(_ + _.getCost())
-          monthlyUsage(i) = monthlyUsageSum
-      case "region" =>
-        val locationBills = list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getRegion() == location)
-        for i <- Range(1, 12) do
-          val monthlyUsageSum = locationBills.filter(bill => bill.getMonth() == i).foldLeft(0.0)(_ + _.getCost())
-          monthlyUsage(i) = monthlyUsageSum
-
-    result = monthlyUsage.toString()
-    result
-
-
+  /*
+  Il seguente metodo effettua le previsioni per un singolo utente con riferimento ad un certo anno e con riferimento ad una certa utenza
+  */
   def makeIndividualPrediction(userID: String, usageType: String, year: Int): String =
     var result = ""
     val annualUsage: LinkedHashMap[Int, Double] = LinkedHashMap()
     val annualCost: LinkedHashMap[Int, Double] = LinkedHashMap()
 
-    list.filter(bill => bill.getUserID() == userID).foreach(bill => annualUsage(bill.getYear()) = 0.0)
-    list.filter(bill => bill.getUserID() == userID).foreach(bill => annualCost(bill.getYear()) = 0.0)
+    individualMapInitialization(annualUsage, userID, usageType)
+    individualMapInitialization(annualCost, userID, usageType)
 
-    annualUsage.keys.foreach(
-      mapYear => annualUsage(mapYear) =
-        list.filter(bill => bill.getUserID() == userID && bill.getYear() == mapYear && bill.getUsageType() == usageType).foldLeft(0.0)(_ + _.getUsage()) /
-          list.count(bill => bill.getUserID() == userID && bill.getYear() == mapYear && bill.getUsageType() == usageType)
-    )
+    fillIndividualUsageCostMap(annualUsage, "usage", userID, usageType)
+    fillIndividualUsageCostMap(annualCost, "cost", userID, usageType)
 
-    annualCost.keys.foreach(
-      mapYear => annualCost(mapYear) =
-        list.filter(bill => bill.getUserID() == userID && bill.getYear() == mapYear && bill.getUsageType() == usageType).foldLeft(0.0)(_ + _.getCost()) /
-          list.count(bill => bill.getUserID() == userID && bill.getYear() == mapYear && bill.getUsageType() == usageType)
-    )
+    annualUsage.toSeq.sortBy(_._1)
+    annualCost.toSeq.sortBy(_._1)
+
+    var percentageUsageVariation = percentageVariation(annualUsage)
+    var percentageCostVariation = percentageVariation(annualCost)
+
+    predictionResult(year,annualUsage, percentageUsageVariation, percentageCostVariation, usageType)
+
+  /*
+  Il seguente metodo effettua le previsioni relativamente ad una certa tipologia di consumi per un certo anno per una specifica località geografica
+  */
+  def makePredictionByLocation(userType: String, usageType: String, year: Int, cityOrRegion: String, cityRegion: String): String =
+
+    var result = ""
+    val annualUsage: LinkedHashMap[Int, Double] = LinkedHashMap()
+    val annualCost: LinkedHashMap[Int, Double] = LinkedHashMap()
+
+    initializationMapByLocation(annualUsage, userType, usageType, cityOrRegion, cityRegion)
+    initializationMapByLocation(annualCost, userType, usageType, cityOrRegion, cityRegion)
+
+    fillUsageCostMapByLocation(annualUsage,usageType,"usage",userType, cityOrRegion, cityRegion)
+    fillUsageCostMapByLocation(annualCost, usageType, "cost", userType, cityOrRegion, cityRegion)
+
     annualUsage.toSeq.sortBy(_._1)
     annualCost.toSeq.sortBy(_._1)
 
     var percentageUsageVariation = annualUsage.values.foldLeft[Double](0.0)(_ + _) / annualUsage.keys.size
     var percentageCostVariation = annualCost.values.foldLeft[Double](0.0)(_ + _) / annualCost.keys.size
 
-    val lengthYearPrediction = year - annualUsage.keys.head
-    if (lengthYearPrediction <= annualUsage.keys.size) then
-      result = "Your usage for " + usageType + " is not supposed to change for " + year
-    else
-      println(annualUsage)
-      for i <- Range(0,lengthYearPrediction - annualUsage.keys.size) do
-        percentageCostVariation = percentageUsageVariation + Random.nextDouble()/100
-        percentageUsageVariation = percentageUsageVariation + Random.nextDouble()/100
-        result = s"Year: ${year}\nPredicted usage variation: ${percentageUsageVariation}\nPredicted cost variation: ${percentageCostVariation}"
+    predictionResult(year,annualUsage, percentageUsageVariation, percentageCostVariation, usageType)
 
+
+
+  /*
+  Il seguente metodo privato ritorna il risultato della previsione richiesta
+  */
+  private def predictionResult(year: Int, map: LinkedHashMap[Int, Double], percentageUsageVariation: Double, percentageCostVariation: Double, usageType: String): String =
+    var result = ""
+    var costVariation = percentageCostVariation
+    var usageVariation = percentageUsageVariation
+    val lengthYearPrediction = year - map.keys.head
+    if (lengthYearPrediction <= map.keys.size) then
+      result = "Your usage and cost for " + usageType + " is not supposed to change for " + year
+    else
+      for i <- Range(0,lengthYearPrediction - map.keys.size) do
+        costVariation = costVariation + Random.nextDouble()/100
+        usageVariation = usageVariation + Random.nextDouble()/100
+        result = s"Year: ${year}\nPredicted usage variation: ${usageVariation}\nPredicted cost variation: ${costVariation}"
     result
 
 
 
+  /*
+  Il seguente metodo privato ritorna la variazione percentuale di costi o consumi su base annuale.
+  La variazione è calcolata sulla mappa anno-consumi/costi che viene fornita in input
+  */
+  private def percentageVariation(map: LinkedHashMap[Int, Double]): Double =
+    map.values.foldLeft(0.0)(_ + _) / map.keys.size
 
-  def makePredictionByLocation(userID: String, userType: String, usageType: String, year: Int, locationType: String, location: String): String =
-    "makePredictionByLocation"
+
+  /*
+  Il seguente metodo inizializza la mappa anno-consumi/costi passata come argomento, filtrando per singolo utente, di modo da poter essere utilizzata nelle previsioni
+  */
+  private def individualMapInitialization(map: LinkedHashMap[Int, Double], userID: String, usageType: String): Unit =
+    list.filter(bill => bill.getUserID() == userID && bill.getUsageType() == usageType).foreach(bill => map(bill.getYear()) = 0.0)
+
+  /*
+  Il seguente metodo inizializza la mappa anno-consumi/costi passata come argomento filtrando per la tipologia di utente, la tipologia di consumo e la località geografica
+  */
+  private def initializationMapByLocation(map: LinkedHashMap[Int, Double], userType: String, usageType: String, cityOrRegion: String, cityRegion: String): Unit =
+    cityOrRegion match
+      case "city" =>
+        list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getCity() == cityRegion)
+          .foreach(bill => map(bill.getYear()) = 0.0)
+      case "region" =>
+        list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getRegion() == cityRegion)
+          .foreach(bill => map(bill.getYear()) = 0.0)
+
+
+  /*
+  Il seguente metodo riempie la mappa anno-consumi/costi relativa ai costi e ai consumi individuali annuali con la media dei consumi/costi per ogni anno
+  */
+  private def fillIndividualUsageCostMap(map: LinkedHashMap[Int, Double], usageOrCost: String, userID: String, usageType: String): Unit =
+    usageOrCost match
+      case "usage" =>
+        map.keys.foreach(
+          mapYear => map(mapYear) =
+            list.filter(bill => bill.getUserID() == userID && bill.getYear() == mapYear && bill.getUsageType() == usageType).foldLeft(0.0)(_ + _.getUsage()) /
+              list.count(bill => bill.getUserID() == userID && bill.getYear() == mapYear && bill.getUsageType() == usageType)
+        )
+      case "cost" =>
+        map.keys.foreach(
+          mapYear => map(mapYear) =
+            list.filter(bill => bill.getUserID() == userID && bill.getYear() == mapYear && bill.getUsageType() == usageType).foldLeft(0.0)(_ + _.getUsage()) /
+              list.count(bill => bill.getUserID() == userID && bill.getYear() == mapYear && bill.getUsageType() == usageType)
+        )
+
+  /*
+  Il seguente metodo riempie la mappa anno-consumi/costi relativa ai costi e consumi per una specifica utenza per una specifica località geografica con la media dei consumi/costi per ogni anno
+  */
+  private def fillUsageCostMapByLocation(map: LinkedHashMap[Int, Double], usageType: String, usageOrCost: String, userType: String, cityOrRegion: String, cityRegion: String): Unit =
+    cityOrRegion match
+      case "city" =>
+        usageOrCost match
+          case "usage" =>
+            map.keys.foreach(
+              mapYear => map(mapYear) =
+                list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getYear() == mapYear && bill.getCity() == cityRegion).foldLeft(0.0)(_ + _.getUsage()) /
+                  list.count(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getYear() == mapYear && bill.getCity() == cityRegion)
+            )
+          case "cost" =>
+            map.keys.foreach(
+              mapYear => map(mapYear) =
+                list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getYear() == mapYear && bill.getCity() == cityRegion).foldLeft(0.0)(_ + _.getCost()) /
+                  list.count(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getYear() == mapYear && bill.getCity() == cityRegion)
+            )
+      case "region" =>
+        usageOrCost match
+          case "usage" =>
+            map.keys.foreach(
+              mapYear => map(mapYear) =
+                list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getYear() == mapYear && bill.getRegion() == cityRegion).foldLeft(0.0)(_ + _.getUsage()) /
+                  list.count(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getYear() == mapYear && bill.getRegion() == cityRegion)
+            )
+          case "cost" =>
+            map.keys.foreach(
+              mapYear => map(mapYear) =
+                list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getYear() == mapYear && bill.getRegion() == cityRegion).foldLeft(0.0)(_ + _.getCost()) /
+                  list.count(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getYear() == mapYear && bill.getRegion() == cityRegion)
+            )
+
+
+  /*
+  Questo metodo privato ritorna le bollette relative allo specifico utente per la specifica utenza
+  */
+  private def getBillsByUserIDAndUsageType(userID: String, usageType: String): List[Bill] =
+    list.filter(bill => bill.getUserID() == userID && bill.getUsageType() == usageType)
+
+
+
+  /*
+  Questo metodo ritorna le bollette relative ad una specifica tipologia di utenti in una certa località geografica con riferimento ad una specifica utenza
+  */
+  private def getBillsByCityOrRegion(userType: String, usageType: String, cityOrRegion: String, cityRegion: String): List[Bill] =
+    var bills: List[Bill] = List()
+    cityOrRegion match
+      case "city" =>
+        bills = list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getCity() == cityRegion)
+      case "region" =>
+        bills = list.filter(bill => bill.getUserType() == userType && bill.getUsageType() == usageType && bill.getRegion() == cityRegion)
+
+    bills
+
+
+
+  /*
+  Questo metodo opera un raggruppamento per mesi riportando una mappa anno-consumi/costi in cui i consumi/costi sono la somma di tutti i valori relativi ad una certa località geografica
+  */
+  private def monthlyUsageOrCost(userType: String, usageType: String, cityOrRegion: String, cityRegion: String, usageOrCost: String): LinkedHashMap[Int, Double] =
+    val monthlyUsageOrCost: LinkedHashMap[Int, Double] = LinkedHashMap()
+    val cityOrRegionBills = getBillsByCityOrRegion(userType, usageType, cityOrRegion, cityRegion)
+    usageOrCost match
+      case "usage" =>
+        for i <- Range(1,12) do
+          val monthlyUsageSum = cityOrRegionBills.filter(bill => bill.getMonth() == i).foldLeft(0.0)(_ + _.getUsage())
+          monthlyUsageOrCost(i) = monthlyUsageSum
+      case "cost" =>
+        for i <- Range(1,12) do
+          val monthlyCostSum = cityOrRegionBills.filter(bill => bill.getMonth() == i).foldLeft(0.0)(_ + _.getCost())
+          monthlyUsageOrCost(i) = monthlyCostSum
+
+    monthlyUsageOrCost
+
+
+  /*
+  Questo metodo privato ritorna semplicemente i risultati dell'analisi su consumi/costi per una certa bolletta
+  */
+  private def composeUsageOrCostInformation(bill: Bill, requestedInformation: String): String =
+    var result = ""
+    requestedInformation match
+      case "cost" =>
+        result = "cost: " + bill.getCost() + "\nmonth: " + bill.getMonth() + "\nyear: " + bill.getYear()+"\n"
+      case "usage" =>
+        result = "usage: " + bill.getUsage() + "\nmonth: " + bill.getMonth() + "\nyear: " + bill.getYear()+"\n"
+
+    result
--- src/test/scala/billOperations/BillOperationsTest.scala
+++ src/test/scala/billOperations/BillOperationsTest.scala
@@ -5,24 +5,30 @@ import org.scalatest.funsuite.AnyFunSuite
 class BillOperationsTest extends AnyFunSuite:
 
   test("Usage should be present") {
-    val result = BillOperations.getIndividualCost("AntonioIannotta","water")
+    val result = BillOperations.getIndividualCostOrUsage("AntonioIannotta","water", "usage")
     assert(!result.isEmpty)
     println(result)
   }
 
   test("Usage for user is not present!") {
-    val result = BillOperations.getIndividualCost("AntonioIannotta", "heat")
+    val result = BillOperations.getIndividualCostOrUsage("AntonioIannotta", "heat", "cost")
     assert(result.isEmpty)
   }
 
   test("Usage for a certain location is present!") {
-    val result = BillOperations.getUsageByLocation("private","water","Emilia-Romagna","region")
+    val result = BillOperations.getUsageOrCostByLocation("private","water","Emilia-Romagna","region", "usage")
     assert(!result.isEmpty)
     println(result)
   }
 
-  test("Usage is not supposed to change!") {
+  test("Usage and cost are not supposed to change!") {
     val result = BillOperations.makeIndividualPrediction("AntonioIannotta","water",2024)
-    assert(result == "Your usage for water is not supposed to change for 2024")
+    assert(result == "Your usage and cost for water is not supposed to change for 2024")
+    println(result)
+  }
+
+  test("Usage and cost are supposed to change!") {
+    val result = BillOperations.makeIndividualPrediction("AntonioIannotta","water",2026)
+    assert(result != "Your usage and cost for water is not supposed to change for 2024")
     println(result)
   }
