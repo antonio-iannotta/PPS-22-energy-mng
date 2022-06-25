2456d1b22770f9a2156db38e6b6fe68eb7342ad3
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-06-25 18:25:05 +0200

    Commit: Refactoring codice Registration

--- src/test/scala/registration/RegistrationTest.scala
+++ src/test/scala/registration/RegistrationTest.scala
@@ -5,8 +5,8 @@ import org.scalatest.funsuite.AnyFunSuite
 class RegistrationTest extends AnyFunSuite:
 
     test("Napoli is not in Lombardia") {
-      assert(registration.Registration.signUP("Demetrio","pippo","private","Napoli","Lombardia") == "error!")
+      assert(registration.Registration.signUP("Demetrio","pippo",0,"Lombardia","Napoli") == "error!")
     }
     test("Milano is in Lombardia") {
-      assert(registration.Registration.signUP("Demetrio","pippo","private","Milano","Lombardia") == "success!")
+      assert(registration.Registration.signUP("Demetrio","pippo",1,"Lombardia","Milano") == "success!")
     }
