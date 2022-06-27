af37ca8cbbd73b13ce52f4c3936240a6e2b549b4
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-06-27 11:37:45 +0200

    Commit: workspace-repaired

---
+++ src/test/scala/billBuilder/registration/RegistrationChecker.scala
@@ -0,0 +1,61 @@
+package registration
+
+import scala.collection.mutable.LinkedHashMap
+
+class RegistrationChecker(private val userID: String, private val password: String, private val userType: Int, private val region: String, private val city: String):
+
+  val regionCityMap: LinkedHashMap[String, List[String]] = LinkedHashMap()
+  regionCityMap("Abruzzo") = Regions.AbruzzoCities
+  regionCityMap("Basilicata") = Regions.BasilicataCities
+  regionCityMap("Calabria") = Regions.CalabriaCities
+  regionCityMap("Campania") = Regions.CampaniaCities
+  regionCityMap("EmiliaRomagna") = Regions.EmiliaRomagnaCities
+  regionCityMap("FVG") = Regions.FVGCities
+  regionCityMap("Lazio") = Regions.LazioCities
+  regionCityMap("Liguria") = Regions.LiguriaCities
+  regionCityMap("Lombardia") = Regions.LombardiaCities
+  regionCityMap("Marche") = Regions.MarcheCities
+  regionCityMap("Molise") = Regions.MoliseCities
+  regionCityMap("Piemonte") = Regions.PiemonteCities
+  regionCityMap("Puglia") = Regions.PugliaCities
+  regionCityMap("Sardegna") = Regions.SardegnaCities
+  regionCityMap("Sicilia") = Regions.SiciliaCities
+  regionCityMap("Toscana") = Regions.ToscanaCities
+  regionCityMap("TAA") = Regions.TAACities
+  regionCityMap("Umbria") = Regions.UmbriaCities
+  regionCityMap("VAC") = Regions.VACities
+  regionCityMap("Veneto") = Regions.VenetoCities
+
+  def checkFields(userID: String, password: String, userType: Int, region: String, city: String): String =
+
+    val userCheck = ErrorCodeHandler.registrationHandler(checkUserID(userID))
+    val passwordCheck = ErrorCodeHandler.registrationHandler(checkPassword(password))
+    val userTypeCheck = ErrorCodeHandler.registrationHandler(checkUserType(userType))
+    val cityCheck = ErrorCodeHandler.registrationHandler(checkCityRegionMatch(city, region))
+    val checkResponseList = List(userCheck, passwordCheck, userTypeCheck, cityCheck).filter(str => str != "OK")
+
+    if checkResponseList.isEmpty then "OK"
+    else checkResponseList.head
+
+  private def checkUserID(userID: String): String =
+    userID match
+      case id if id == "" => "REGISTRATION_USERID_1"
+      case id if id.length > 20 => "REGISTRATION_USERID_2"
+      case id if id.length < 6 => "REGISTRATION_USERID_3"
+      case _ => "OK"
+
+  private def checkPassword(password: String): String =
+    password match
+      case pwd if pwd == "" => "REGISTRATION_PASSWORD_1"
+      case pwd if pwd.length > 20 => "REGISTRATION_PASSWORD_2"
+      case pwd if pwd.length < 6 => "REGISTRATION_PASSWORD_3"
+      case _ => "OK"
+
+  private def checkUserType(userType: Int): String =
+    if 0 to 1 contains userType then "OK"
+    else "REGISTRATION_USERTYPE_1"
+
+
+  private def checkCityRegionMatch(city: String, region: String): String =
+    if regionCityMap(region).contains(city) then "OK"
+    else "REGISTRATION_CITY_1"
