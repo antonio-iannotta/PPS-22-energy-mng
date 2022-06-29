569cfc8c550b5e0187089fbe0a3b0d3ee3ca19f5
carlo di raddo <carlo.diraddo@gmail.com>
2022-06-29 10:40:03 +0200

    Commit: updates

--- src/test/scala/user/UserTest.scala
+++ src/test/scala/user/UserTest.scala
@@ -5,8 +5,9 @@ import org.scalatest.funsuite.AnyFunSuite
 class UserTest extends AnyFunSuite:
 
   test("All fields are correct") {
-    val UserClassTest = new User("codiceFiscale", "regione", "città", "privato")
+    val UserClassTest = new User("codiceFiscale", "123456", "regione", "città", "privato")
     assert(UserClassTest.getUserID() == "codiceFiscale")
+    assert(UserClassTest.getPassword() == "123456")
     assert(UserClassTest.getRegion() == "regione")
     assert(UserClassTest.getCity() == "città")
     assert(UserClassTest.getUserType() == "privato")
@@ -15,6 +16,7 @@ class UserTest extends AnyFunSuite:
   test("All fields are empty"){
     val UserClassTest = new User("", "", "", "")
     assert(UserClassTest.getUserID() != "codiceFiscale")
+    assert(UserClassTest.getPassword() != "123456")
     assert(UserClassTest.getRegion() != "regione")
     assert(UserClassTest.getCity() != "città")
     assert(UserClassTest.getUserType() != "privato")
@@ -23,6 +25,7 @@ class UserTest extends AnyFunSuite:
   test("All fields are null"){
     val UserClassTest = new User(null, null, null, null)
     assert(UserClassTest.getUserID() == null)
+    assert(UserClassTest.getPassword() == null)
     assert(UserClassTest.getRegion() == null)
     assert(UserClassTest.getCity() == null)
     assert(UserClassTest.getUserType() == null)
