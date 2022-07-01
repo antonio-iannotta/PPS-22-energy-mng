c58d6400bbbe8f689ca3d4c558ef0fcaf831cb4c
carlo di raddo <carlo.diraddo@gmail.com>
2022-07-01 10:42:19 +0200

    Commit: User class updated

--- src/main/scala/user/User.scala
+++ src/main/scala/user/User.scala
@@ -1,48 +1,45 @@
-package user
-
 import billOperations.BillOperations
 
-class User (val userID: String, val password: String, val region: String,val city: String,val userType: String):
-  
-  def getUserID(): String = userID
-  
-  def getRegion(): String = region
-  
-  def getUserType(): String = userType
-  
-  def getCity(): String = city
-  
-  def getPassword(): String = password
+class User (private val _userID: String, private val _password: String, private val _region: String, private val _city: String, private val _userType: String):
+
+  def getUserID: String = _userID
+
+  def getRegion: String = _region
+
+  def getUserType: String = _userType
+
+  def getCity: String = _city
+
+  def getPassword: String = _password
 
   def printUser(): Unit =
-    println("UserID: " + getUserID() + "\nRegion: " + getRegion() + "\nCity: " + getCity() +"\nUserType: " + getUserType())
+    println("UserID: " + getUserID + "\nRegion: " + getRegion + "\nCity: " + getCity +"\nUserType: " + getUserType)
 
 
   def getCost(usageType: String): Unit =
-    println(BillOperations.getIndividualCostOrUsage(getUserID(),usageType,"cost"))
+    println(BillOperations.getIndividualCostOrUsage(getUserID,usageType,"cost"))
 
   def getUsage(usageType: String): Unit =
-    println(BillOperations.getIndividualCostOrUsage(getUserID(),usageType,"usage"))
+    println(BillOperations.getIndividualCostOrUsage(getUserID,usageType,"usage"))
 
-  def getCostByCity(city: String = getCity(), usageType: String): Unit =
-    println(BillOperations.getUsageOrCostByLocation(getUserType(),usageType,city,"city","cost"))
+  def getCostByCity(city: String = getCity, usageType: String): Unit =
+    println(BillOperations.getUsageOrCostByLocation(getUserType,usageType,city,"city","cost"))
 
-  def getUsageByCity(city: String = getCity(), usageType: String): Unit =
-    println(BillOperations.getUsageOrCostByLocation(getUserType(),usageType,city,"city","usage"))
+  def getUsageByCity(city: String = getCity, usageType: String): Unit =
+    println(BillOperations.getUsageOrCostByLocation(getUserType,usageType,city,"city","usage"))
 
-  def getCostByRegion(region: String = getRegion(), usageType: String): Unit =
-    println(BillOperations.getUsageOrCostByLocation(getUserType(),usageType,region,"region","cost"))
+  def getCostByRegion(region: String = getRegion, usageType: String): Unit =
+    println(BillOperations.getUsageOrCostByLocation(getUserType,usageType,region,"region","cost"))
 
-  def getUsageByRegion(region: String = getRegion(), usageType: String): Unit =
-    println(BillOperations.getUsageOrCostByLocation(getUserType(),usageType,region,"region","usage"))
-    
+  def getUsageByRegion(region: String = getRegion, usageType: String): Unit =
+    println(BillOperations.getUsageOrCostByLocation(getUserType,usageType,region,"region","usage"))
+
   def makeIndividualPrediction(usageType: String, year: Int): Unit =
-    println(BillOperations.makeIndividualPrediction(getUserID(), usageType, year))
-    
-  def makePredictionByCity(usageType: String, year: Int, city: String = getCity()): Unit =
-    println(BillOperations.makePredictionByLocation(getUserType(), usageType, year,"city", city))
-    
-  def makePredictionByRegion(usageType: String, year: Int, region: String = getRegion()): Unit =
-    println(BillOperations.makePredictionByLocation(getUserType(), usageType, year,"region", region))
+    println(BillOperations.makeIndividualPrediction(getUserID, usageType, year))
 
+  def makePredictionByCity(usageType: String, year: Int, city: String = getCity): Unit =
+    println(BillOperations.makePredictionByLocation(getUserType, usageType, year,"city", city))
+
+  def makePredictionByRegion(usageType: String, year: Int, region: String = getRegion): Unit =
+    println(BillOperations.makePredictionByLocation(getUserType, usageType, year,"region", region))
 
