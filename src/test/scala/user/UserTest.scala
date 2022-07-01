6a1bad92db0468928f0b10ea8bff6592e9444fb9
carlo di raddo <carlo.diraddo@gmail.com>
2022-07-01 10:49:09 +0200

    Commit: UserTest class updated

--- src/test/scala/user/UserTest.scala
+++ src/test/scala/user/UserTest.scala
@@ -6,27 +6,27 @@ class UserTest extends AnyFunSuite:
 
   test("All fields are correct") {
     val UserClassTest = User("codiceFiscale", "123456", "regione", "città", "privato")
-    assert(UserClassTest.getUserID() == "codiceFiscale")
-    assert(UserClassTest.getPassword() == "123456")
-    assert(UserClassTest.getRegion() == "regione")
-    assert(UserClassTest.getCity() == "città")
-    assert(UserClassTest.getUserType() == "privato")
+    assert(UserClassTest.getUserID == "codiceFiscale")
+    assert(UserClassTest.getPassword == "123456")
+    assert(UserClassTest.getRegion == "regione")
+    assert(UserClassTest.getCity == "città")
+    assert(UserClassTest.getUserType == "privato")
   }
 
   test("All fields are empty"){
     val UserClassTest = User("","", "", "", "")
-    assert(UserClassTest.getUserID() != "codiceFiscale")
-    assert(UserClassTest.getPassword() != "123456")
-    assert(UserClassTest.getRegion() != "regione")
-    assert(UserClassTest.getCity() != "città")
-    assert(UserClassTest.getUserType() != "privato")
+    assert(UserClassTest.getUserID != "codiceFiscale")
+    assert(UserClassTest.getPassword != "123456")
+    assert(UserClassTest.getRegion != "regione")
+    assert(UserClassTest.getCity != "città")
+    assert(UserClassTest.getUserType != "privato")
   }
 
   test("All fields are null"){
     val UserClassTest = User(null, null,null, null, null)
-    assert(UserClassTest.getUserID() == null)
-    assert(UserClassTest.getPassword() == null)
-    assert(UserClassTest.getRegion() == null)
-    assert(UserClassTest.getCity() == null)
-    assert(UserClassTest.getUserType() == null)
+    assert(UserClassTest.getUserID == null)
+    assert(UserClassTest.getPassword == null)
+    assert(UserClassTest.getRegion == null)
+    assert(UserClassTest.getCity == null)
+    assert(UserClassTest.getUserType == null)
   }
