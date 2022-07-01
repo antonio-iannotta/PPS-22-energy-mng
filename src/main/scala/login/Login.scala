aa41c56cae9bfdb5dd126a8726f58fe0960f1009
carlo di raddo <carlo.diraddo@gmail.com>
2022-07-01 10:41:08 +0200

    Commit: Login class updated

--- src/main/scala/login/Login.scala
+++ src/main/scala/login/Login.scala
@@ -4,18 +4,15 @@ import mongoDriver.MongoDB._
 import registration.MD5
 import user.User
 
-object Login extends App:
-  def signIN(userID: String, password: String): User =
-    var user: User = null
+object Login:
+  def signIN(userID: String, password: String): Option[User] =
+    var user: Option[User] = Option.empty
     var checkerResponse: String = ""
-    var resultSignInOperation = ""
     val loginChecker = LoginChecker(userID, password)
-    
+
     checkerResponse = loginChecker.checkFields(userID, password)
     if checkerResponse == "OK" then
       val userList = retrieveUsers()
-      user = userList.filter(user => user.getUserID() == userID && user.getPassword() == MD5.md5HashPassword(password)).head
+      user = Option.apply(userList.filter(user => user.getUserID() == userID && user.getPassword() == MD5.md5HashPassword(password)).head)
 
     user
-    
-  signIN("andreacato", "password")
