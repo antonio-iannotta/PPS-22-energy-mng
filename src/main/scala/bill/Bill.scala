bcdac62d46bae44ee82f94d1def39471ae56dad2
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-07-04 11:00:48 +0200

    Commit: Bill Updates

--- src/main/scala/bill/Bill.scala
+++ src/main/scala/bill/Bill.scala
@@ -4,23 +4,22 @@ class Bill(private val _billID: String, private val _userID: String, private val
            private val _usageType: String, private val _usage: Double, private val _cost: Double,
            private val _month: Int, private val _year: Int, private val _city: String, private val _region: String):
 
-  def getUserID: String = _userID
+  def userID: String = _userID
 
-  def getRegion: String = _region
+  def region: String = _region
 
-  def getUserType: String = _userType
+  def userType: String = _userType
 
-  def getBillID: String = _billID
+  def billID: String = _billID
 
-  def getCity: String = _city
+  def city: String = _city
 
-  def getUsageType: String = _usageType
+  def usageType: String = _usageType
 
-  def getUsage: Double = _usage
+  def usage: Double = _usage
 
-  def getCost: Double = _cost
+  def cost: Double = _cost
 
-  def getMonth: Int = _month
+  def month: Int = _month
 
-  def getYear: Int = _year
-
+  def year: Int = _year
