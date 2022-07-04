a0e23c24064348c81773747b57519dba47b2f787
carlo di raddo <carlo.diraddo@gmail.com>
2022-07-04 11:11:05 +0200

    Commit: Login Updated

--- src/main/scala/login/Login.scala
+++ src/main/scala/login/Login.scala
@@ -6,13 +6,8 @@ import user.User
 
 object Login:
   def signIN(userID: String, password: String): Option[User] =
-    var user: Option[User] = Option.empty
-    var checkerResponse: String = ""
-    val loginChecker = LoginChecker(userID, password)
-
-    checkerResponse = loginChecker.checkFields(userID, password)
-    if checkerResponse == "OK" then
-      val userList = retrieveUsers()
-      user = Option.apply(userList.filter(user => user.getUserID == userID && user.getPassword == MD5.md5HashPassword(password)).head)
-
-    user
+    LoginChecker(userID, password).checkFields(userID, password) match
+      case "OK" =>
+        Option.apply(retrieveUsers().filter(user => user.getUserID == userID && user.getPassword == MD5.md5HashPassword(password)).head)
+      case _ =>
+        Option.empty[User]
