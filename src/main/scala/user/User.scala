dd7a0ac66f3f24567c665cbaa396e23781055930
carlo di raddo <carlo.diraddo@gmail.com>
2022-07-04 11:10:31 +0200

    Commit: User Updated

--- src/main/scala/user/User.scala
+++ src/main/scala/user/User.scala
@@ -4,44 +4,43 @@ import billOperations.BillOperations
 
 class User (private val _userID: String, private val _password: String, private val _region: String, private val _city: String, private val _userType: String):
 
-  def getUserID: String = _userID
+  def userID: String = _userID
 
-  def getRegion: String = _region
+  def region: String = _region
 
-  def getUserType: String = _userType
+  def userType: String = _userType
 
-  def getCity: String = _city
+  def city: String = _city
 
-  def getPassword: String = _password
+  def password: String = _password
 
   def printUser(): Unit =
-    println("UserID: " + getUserID + "\nRegion: " + getRegion + "\nCity: " + getCity +"\nUserType: " + getUserType)
+    println("UserID: " + userID + "\nRegion: " + region + "\nCity: " + city +"\nUserType: " + userType)
 
 
   def getCost(usageType: String): Unit =
-    println(BillOperations.getIndividualCostOrUsage(getUserID,usageType,"cost"))
+    println(BillOperations.getIndividualCostOrUsage(userID,usageType,"cost"))
 
   def getUsage(usageType: String): Unit =
-    println(BillOperations.getIndividualCostOrUsage(getUserID,usageType,"usage"))
+    println(BillOperations.getIndividualCostOrUsage(userID,usageType,"usage"))
 
-  def getCostByCity(city: String = getCity, usageType: String): Unit =
-    println(BillOperations.getUsageOrCostByLocation(getUserType,usageType,city,"city","cost"))
+  def getCostByCity(city: String = city, usageType: String): Unit =
+    println(BillOperations.getUsageOrCostByLocation(userType,usageType,city,"city","cost"))
 
-  def getUsageByCity(city: String = getCity, usageType: String): Unit =
-    println(BillOperations.getUsageOrCostByLocation(getUserType,usageType,city,"city","usage"))
+  def getUsageByCity(city: String = city, usageType: String): Unit =
+    println(BillOperations.getUsageOrCostByLocation(userType,usageType,city,"city","usage"))
 
-  def getCostByRegion(region: String = getRegion, usageType: String): Unit =
-    println(BillOperations.getUsageOrCostByLocation(getUserType,usageType,region,"region","cost"))
+  def getCostByRegion(region: String = region, usageType: String): Unit =
+    println(BillOperations.getUsageOrCostByLocation(userType,usageType,region,"region","cost"))
 
-  def getUsageByRegion(region: String = getRegion, usageType: String): Unit =
-    println(BillOperations.getUsageOrCostByLocation(getUserType,usageType,region,"region","usage"))
+  def getUsageByRegion(region: String = region, usageType: String): Unit =
+    println(BillOperations.getUsageOrCostByLocation(userType,usageType,region,"region","usage"))
 
   def makeIndividualPrediction(usageType: String, year: Int): Unit =
-    println(BillOperations.makeIndividualPrediction(getUserID, usageType, year))
+    println(BillOperations.makeIndividualPrediction(userID, usageType, year))
 
-  def makePredictionByCity(usageType: String, year: Int, city: String = getCity): Unit =
-    println(BillOperations.makePredictionByLocation(getUserType, usageType, year,"city", city))
+  def makePredictionByCity(usageType: String, year: Int, city: String = city): Unit =
+    println(BillOperations.makePredictionByLocation(userType, usageType, year,"city", city))
 
-  def makePredictionByRegion(usageType: String, year: Int, region: String = getRegion): Unit =
-    println(BillOperations.makePredictionByLocation(getUserType, usageType, year,"region", region))
-
+  def makePredictionByRegion(usageType: String, year: Int, region: String = region): Unit =
+    println(BillOperations.makePredictionByLocation(userType, usageType, year,"region", region))
