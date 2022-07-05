8ec2fe5e95e52ebb85f35d15d0907fead8c320a4
carlo di raddo <carlo.diraddo@gmail.com>
2022-07-05 10:13:36 +0200

    Commit: Dashboard update

--- src/main/scala/dashboard/Dashboard.scala
+++ src/main/scala/dashboard/Dashboard.scala
@@ -10,40 +10,40 @@ case class Dashboard(private val user: User):
     var exit = true
 
     while(exit)
-          println("Selezionare voce desiderata:")
-          println("1) visualizza costi utente")
-          println("2) visualizza utilizzi utente")
-          println("3) visualizza costi città")
-          println("4) visualizza utilizzi città")
-          println("5) visualizza costi regione")
-          println("6) visualizza utilizzi regione")
-          println("7) previsione individuale")
-          println("8) previsione città")
-          println("9) previsione regione")
-          println("10) exit")
+      println("Selezionare voce desiderata:")
+      println("1) visualizza costi utente")
+      println("2) visualizza utilizzi utente")
+      println("3) visualizza costi città")
+      println("4) visualizza utilizzi città")
+      println("5) visualizza costi regione")
+      println("6) visualizza utilizzi regione")
+      println("7) previsione individuale")
+      println("8) previsione città")
+      println("9) previsione regione")
+      println("10) exit")
 
-    var selection = scala.io.StdIn.readInt()
+      var selection = scala.io.StdIn.readInt()
 
-    selection match
-              case 1 => user.getCost(user.userType)
-              case 2 => user.getUsage(user.userType)
-              case 3 => CityCostChoiceHandler.choiceHandler(user)
-              case 4 => CityUsageChoiceHandler.choiceHandler(user)
-              case 5 => RegionCostChoiceHandler.choiceHandler(user)
-              case 6 => RegionUsageChoiceHandler.choiceHandler(user)
+      selection match
+                case 1 => user.getCost(user.userType)
+                case 2 => user.getUsage(user.userType)
+                case 3 => CityCostChoiceHandler.choiceHandler(user)
+                case 4 => CityUsageChoiceHandler.choiceHandler(user)
+                case 5 => RegionCostChoiceHandler.choiceHandler(user)
+                case 6 => RegionUsageChoiceHandler.choiceHandler(user)
 
-              case 7 => println("Inserire l'anno d'interesse")
+                case 7 => println("Inserire l'anno d'interesse")
                         var year = scala.io.StdIn.readInt()
                         user.makeIndividualPrediction(user.userType,year)
 
-              case 8 => println("Inserire l'anno d'interesse")
+                case 8 => println("Inserire l'anno d'interesse")
                         var year = scala.io.StdIn.readInt()
                         user.makePredictionByCity(user.userType,year)
 
-              case 9 => println("Inserire l'anno d'interesse")
+                case 9 => println("Inserire l'anno d'interesse")
                         var year = scala.io.StdIn.readInt()
                         user.makePredictionByRegion(user.userType,year)
 
-              case 10 => exit = false
+                case 10 => exit = false
 
-              case _ => println("Scelta non valida!")
+                case _ => println("Scelta non valida!")
