198142d6181c9137842a954144afdd47228be56e
demetrio.andriani <demetrio.andriani@studio.unibo.it>
2022-07-05 16:50:42 +0200

    Commit: update message Main

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -5,25 +5,27 @@ import login.Login
 import user.User
 import registration.Registration
 
-object Main extends App:
+object Main extends App :
 
-  var selection = 0
-  while !(1 to 2 contains selection) do
-    println("Salve, che operazione vuoi effettuare? Digita: \n 1) Registrazione \n 2) Login")
-    try
-      selection = scala.io.StdIn.readInt()
-      selection match
-        case 1 =>
-          val responseMessage = callRegistration()
-          responseMessage match
-            case "OK" => println("Registrazione avvenuta con successo!")
-            case _ => println(responseMessage)
+  var exit = true
+  while exit do
+    println("Salve, che operazione vuoi effettuare? Digita: \n 1) Registrazione \n 2) Login \n 3) Esci")
+    val selection = scala.io.StdIn.readLine()
+    selection match
+      case "1" =>
+        val responseMessage = callRegistration()
+        responseMessage match
+          case "OK" => println("Registrazione avvenuta con successo!")
+          case _ => println(responseMessage)
 
-        case 2 =>
-          val user = callLogin()
-          Dashboard(user.get).view()
-    catch
-      case  _: NumberFormatException => println("Input non valido.")
+      case "2" =>
+        val user = callLogin()
+        Dashboard(user.get).view()
+      case "3" =>
+        exit = false
+        println("Arrivederci, grazie per aver utilizzato Energy Management.")
+      case _ =>
+        println("Input errato, inserisci una possibile scelta.")
 
   def callRegistration(): String =
     println("Registrazione: inserire userID, password, userType, region e city")
@@ -31,14 +33,26 @@ object Main extends App:
     val userID = scala.io.StdIn.readLine()
     println("password: ")
     val password = scala.io.StdIn.readLine()
-    println("user type: ")
-    val userType = scala.io.StdIn.readInt()
+    println("user type: \n 0) Privato \n 1) Azienda ")
+
+    var repeat = true
+    var userType = ""
+    while repeat do
+      userType = scala.io.StdIn.readLine()
+      userType match
+        case "0" =>
+          repeat = false
+        case "1" =>
+          repeat = false
+        case _ =>
+          println("Input errato, digita: \n 0) Privato \n 1) Azienda")
+
     println("region: ")
     val region = scala.io.StdIn.readLine()
     println("city: ")
     val city = scala.io.StdIn.readLine()
 
-    Registration.signUP(userID, password, userType, region, city)
+    Registration.signUP(userID, password, userType.toInt, region, city)
 
   def callLogin(): Option[User] =
     println("Login: inserire userID, password")
