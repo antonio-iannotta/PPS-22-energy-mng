247f9364577e84b34f96aea18bb4ff6363ce50cb
carlo di raddo <carlo.diraddo@gmail.com>
2022-07-06 12:07:25 +0200

    Commit: ChoiceHandler update

--- src/main/scala/presentationLayer/dashboard/choiceHandler/ChoiceHandler.scala
+++ src/main/scala/presentationLayer/dashboard/choiceHandler/ChoiceHandler.scala
@@ -5,87 +5,90 @@ import scala.collection.mutable
 import scala.collection.mutable.*
 
 object ChoiceHandler:
-  override def choiceHandler(user: User, stampa: Int): LinkedHashMap[Int, Double] =
+  override def choiceHandler(user: User, printMsg: Int): LinkedHashMap[Int, Double] =
+    var choice: Int = 0
+    var citySelected: String = ""
+    var regionSelected: String = ""
 
-    stampa match
-      case 1 => stampaClassica
-        val scelta = scala.io.StdIn.readInt()
+    printMsg match
+      case 1 => classicPrint()
+        choice = scala.io.StdIn.readInt()
 
-        scelta match
+        choice match
           case 1 => user.getCost("electricity")
           case 2 => user.getCost("heat")
           case 3 => user.getCost("water")
 
-      case 2 => stampaClassica
-        val scelta = scala.io.StdIn.readInt()
+      case 2 => classicPrint()
+        choice = scala.io.StdIn.readInt()
 
-        scelta match
+        choice match
           case 1 => user.getUsage("electricity")
           case 2 => user.getUsage("heat")
           case 3 => user.getUsage("water")
 
-      case 3 || 4 => println("1) Città di default")
-        println("2) Città a scelta")
-        val scelta = scala.io.StdIn.readLine()
-        scelta match
-          case "1" => scelta = 3
-          case "2" => scelta = 4
+      case 3 | 4 => println("1) Città di default")
+        println("2) Città a choice")
+        val choice_str = scala.io.StdIn.readLine()
+        choice_str match
+          case "1" => choice = 3
+          case "2" => choice = 4
 
-      case 5 || 6 => println("1) Regione di default")
-        println("2) Regione a scelta")
-        val scelta = scala.io.StdIn.readLine()
-        scelta match
-          case "1" => scelta = 5
-          case "2" => scelta = 6
+      case 5 | 6 => println("1) Regione di default")
+        println("2) Regione a choice")
+        val choice_str = scala.io.StdIn.readLine()
+        choice_str match
+          case "1" => choice = 5
+          case "2" => choice = 6
 
-    scelta match
-      case 3 || 5 =>
-        stampaClassica
+      case _ =>
+        println("L'input inserito non è valido.")
+        mutable.LinkedHashMap[Int, Double]()
+
+    choice match
+      case 3 | 5 =>
+        classicPrint()
         val usage = scala.io.StdIn.readLine()
 
         usage match
-          case "1" => scelta match
+          case "1" => choice match
             case 3 => user.getCostByCity(usageType = "electricity")
             case 5 => user.getCostByRegion(usageType = "electricity")
-          case "2" => scelta match
+          case "2" => choice match
             case 3 => user.getCostByCity(usageType = "heat")
             case 5 => user.getCostByRegion(usageType = "heat")
-          case "3" => scelta match
+          case "3" => choice match
             case 3 => user.getCostByCity(usageType = "water")
             case 5 => user.getCostByRegion(usageType = "water")
           case _ =>
             println("L'input inserito non è valido.")
             mutable.LinkedHashMap[Int, Double]()
 
-      case 4 || 6 => scelta match
+      case 4 | 6 => choice match
         case 4 => println("Inserire la città")
-          val citySelected = scala.io.StdIn.readLine()
+          citySelected = scala.io.StdIn.readLine()
         case 6 => println("Inserire la regione")
-          val regionSelected = scala.io.StdIn.readLine()
+          regionSelected = scala.io.StdIn.readLine()
 
-          stampaClassica
+          classicPrint()
           val usage = scala.io.StdIn.readLine()
           usage match
-            case "1" => scelta match
+            case "1" => choice match
               case 4 => user.getCostByCity(citySelected, usageType = "electricity")
               case 6 => user.getCostByRegion(regionSelected, usageType = "electricity")
-            case "2" => scelta match
+            case "2" => choice match
               case 4 => user.getCostByCity(citySelected, usageType = "heat")
               case 6 => user.getCostByRegion(regionSelected, usageType = "heat")
-            case "3" => scelta match
+            case "3" => choice match
               case 4 => user.getCostByCity(citySelected, usageType = "water")
               case 6 => user.getCostByRegion(regionSelected, usageType = "water")
             case _ =>
               println("L'input inserito non è valido.")
               mutable.LinkedHashMap[Int, Double]()
 
-    case _ =>
-    println("L'input inserito non è valido.")
-    mutable.LinkedHashMap[Int, Double]()
 
-    def stampaClassica =
+    def classicPrint(): Unit =
       println("Inserire il consumo d'interesse:")
       println("1) per visualizzare consumi luce")
       println("2) per visualizzare consumi gas")
       println("3) per visualizzare consumi acqua")
-
