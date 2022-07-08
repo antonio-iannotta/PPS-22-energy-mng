9499de5916ab0b1a55872ac2b74e92ecfe18dbcc
carlo di raddo <carlo.diraddo@gmail.com>
2022-07-08 16:08:27 +0200

    Commit: Refactoring

--- src/main/scala/presentationLayer/dashboard/choiceHandler/ChoiceHandler.scala
+++ src/main/scala/presentationLayer/dashboard/choiceHandler/ChoiceHandler.scala
@@ -48,7 +48,6 @@ object ChoiceHandler:
         val year = scala.io.StdIn.readInt()
 
         user.getUsageOrCostByRegionOrCity(cityOrRegionSelected, usage, locationType, usageOrCost, year)
-      case _ => println("Input non valido!")
 
   def individualChoiceHandler(user: User, usageOrCost: String): String =
 
