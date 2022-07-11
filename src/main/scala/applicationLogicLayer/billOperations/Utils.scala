e6fe9df1739709178060e70cd0e247955382d1f3
antonio-iannotta <91571686+antonio-iannotta@users.noreply.github.com>
2022-07-11 10:34:16 +0200

    Update Utils.scala

--- src/main/scala/applicationLogicLayer/billOperations/Utils.scala
+++ src/main/scala/applicationLogicLayer/billOperations/Utils.scala
@@ -282,10 +282,10 @@ object Utils:
     usageOrCost match
 
       case "cost" =>
-        "Cost: " + bill.cost + "€ " + "- " + "Usage type: " + bill.usageType + "- Month: " + bill.month + "- Year: " + bill.year + "\n"
+        "Cost: " + bill.cost + "€" + " - Usage type: " + bill.usageType + " - Month: " + bill.month + " - Year: " + bill.year + "\n"
 
       case "usage" =>
-        "Usage: " + usageFormatter(bill.usageType, bill.usage) + "- Usage type: " + bill.usageType +  "- Month: " + bill.month + "- Year: " + bill.year + "\n"
+        "Usage: " + usageFormatter(bill.usageType, bill.usage) + " - Usage type: " + bill.usageType +  " - Month: " + bill.month + " - Year: " + bill.year + "\n"
 
 
   def usageFormatter(usageType: String, usage: Double): String =
